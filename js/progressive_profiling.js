1f523b1600edc3df27dd14fd56dcd78ead52f2fa
Vasile Chindris <vasile.chindris@amazee.com>
2016-04-26 17:19:44 +0200

    FOR-2006 Added a class on the modal popup and wrapp the button on the form.

--- js/progressive_profiling.js
+++ js/progressive_profiling.js
@@ -4,7 +4,7 @@
       if ($('form#progressive-profiling-form', context).get(0)) {
         var profiling_form = $('form#progressive-profiling-form', context);
         Drupal.CTools.Modal.show('modal-popup-small');
-        $('#modal-content').empty().prepend(profiling_form);
+        $('#modal-content').addClass('modal-content-profiling').empty().prepend(profiling_form);
         $('#modal-title').html(profiling_form.find('#edit-title').text());
         profiling_form.find('#edit-title').remove();
       }
